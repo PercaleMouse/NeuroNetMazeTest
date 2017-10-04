116c0e4e9760db32625216f05418c00fdf1cfc0c
PercaleMouse <user@localhost>
2017-10-04 19:42:07 +0500

    after refactor variables

--- src/neronetmazetester/NewApplication.java
+++ src/neronetmazetester/NewApplication.java
@@ -275,7 +275,7 @@ public class NewApplication extends javax.swing.JFrame {
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         final java.util.Random random = new java.util.Random();
         while (true) {
-            int i = random.nextInt(4);
+            int i = random.nextInt(32);
             System.out.println(Integer.toString(i));
             switch (i) {
                 case 0:
